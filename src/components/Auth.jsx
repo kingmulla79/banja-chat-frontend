5e18fcb0288db1108bca3b61a664774e99fe089b
Thomas <thomasodhiambo800.com>
2023-03-09 01:04:18 +0300

    banja-client

---
+++ src/components/Auth.jsx
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+import signinImage from '../assets/turned-on-android-smartphone.jpg';
+
+const cookies = new Cookies();
+const initialState ={
+        fullName: '',
+        username: '',
+        confirmPassword: '',
+        phoneNumber: '',
+        avatarURL: ''
+}
+
+const Auth = () => {
+    const [form, setForm] = useState(initialState);
+    const [ isSignUp, setIsSignUp] = useState(true);
+
+    const handeleChange = async(e) =>{
+        setForm({...form, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit =  async(e) =>{
+        e.preventDefault();
+        
+        const { username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'http://localhost:5000/auth';
+
+        const { data:{ token, userId, hashedPassword, fullName} } = await axios.post(`${URL}/${isSignUp ? 'signup' : 'login'}`,{
+            username,password, fullName:form.fullName ,phoneNumber,avatarURL,
+        });
+
+        cookies.set('token',token);
+        cookies.set('username',username);
+        cookies.set('fullName',fullName);
+        cookies.set('userId',userId);
+
+        if (isSignUp){
+        cookies.set('phoneNumber',phoneNumber);
+        cookies.set('avatarURL',avatarURL);
+        cookies.set('hashedPassword',hashedPassword);
+        }
+
+        window.location.reload();
+    }
+
+    const switchMode = () =>{
+        setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+    }
+
+    return (
+    <div className='auth__form-container'>
+        <div className='auth__form-container_fields'>
+            <div className='auth__form-container_fields-content'>
+                <p>{isSignUp ? 'Sign Up' : 'Sign In'}</p>
+                <form onSubmit={handleSubmit}>
+                    {isSignUp && (
+                        <div className='auth__form-container_fields-content_input'>
+                            <label htmlFor='fullName'>Full Name</label>
+                            <input
+                                name='fullName' 
+                                type='text'
+                                placeholder='Full Name'
+                                onChange={handeleChange}
+                                required
+                            />
+                        </div>
+                    )}
+                    <div className='auth__form-container_fields-content_input'>
+                        <label htmlFor='fullName'>Username</label>
+                            <input
+                                name='username' 
+                                type='text'
+                                placeholder='Username'
+                                onChange={handeleChange}
+                                required
+                            />
+                    </div>
+                    {isSignUp && (
+                        <div className='auth__form-container_fields-content_input'>
+                            <label htmlFor='fullName'>Phone Number</label>
+                            <input
+                                name='phoneNumber' 
+                                type='text'
+                                placeholder='Phone Number'
+                                onChange={handeleChange}
+                                required
+                            />
+                        </div>
+                    )}
+                    {isSignUp && (
+                        <div className='auth__form-container_fields-content_input'>
+                            <label htmlFor='avatarURL'>Avatar URL</label>
+                            <input
+                                name='avatarURL' 
+                                type='text'
+                                placeholder='Avatar URL'
+                                onChange={handeleChange}
+                                required
+                            />
+                        </div>
+                    )}
+                    <div className='auth__form-container_fields-content_input'>
+                            <label htmlFor='password'>Password</label>
+                            <input
+                                name='password' 
+                                type='password'
+                                placeholder='Password'
+                                onChange={handeleChange}
+                                required
+                            />
+                    </div>
+                    {isSignUp && (
+                        <div className='auth__form-container_fields-content_input'>
+                            <label htmlFor='confirmPassword'>Confirm Password</label>
+                            <input
+                                name='confirmPassword' 
+                                type='password'
+                                placeholder='Confirm Password'
+                                onChange={handeleChange}
+                                required
+                            />
+                        </div>
+                    )}
+                    <div className='auth__form-container_fields-content_button'>
+                        <button>{isSignUp ? 'Sign Up' : 'Sign In'}</button>
+                    </div>
+                </form>
+                <div className='auth__form-container_fields-account'>
+                    <p>
+                        {
+                            isSignUp ?
+                            'Already hane an account?' :
+                            "Don't have an account?"
+                        }
+                        <span onClick={switchMode}>
+                        {
+                            isSignUp ?
+                            'Sign In' :
+                            "Sign Up"
+                        }
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>   
+           <div className='auth__form-container_image'>
+            <img src={signinImage} alt='Sign in image'/>
+           </div>
+    </div>
+  )
+}
+
+export default Auth;
---
+++ src/components/ChannelContainer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { Channel, useChatContext, MessageText, Attachment, MessageInput} from 'stream-chat-react';
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
+
+const ChannelContainer = ({isCreating,setIsCreating,isEditing,setIsEditing,createType}) => {
+  const { channel } = useChatContext();
+
+if(isCreating){
+  return(
+    <div className='channel__container'>
+      <CreateChannel createType={createType} setIsCreating={setIsCreating}/>
+    </div>
+  )
+}
+
+if(isEditing){
+  return(
+    <div className='channel__container'>
+      <EditChannel setIsEditing={setIsEditing}/>
+    </div>
+    )
+  }
+
+  const EmptyState = () => (
+    <div className='channel-empty__container'>
+      <p className='channel-empty__first'>Chat history start</p>
+      <p className='channel-empty__second'>Send message,attachments</p>
+    </div>
+  )
+
+  return (
+    <div>
+      <Channel
+          EmptyStateIndicator={EmptyState}
+          Message={(messageProps, i) => <MessageText key={i} {...messageProps}/>} //message UI
+      >
+        <ChannelInner setIsEditing={setIsEditing}/>
+      </Channel>
+    </div>
+  )
+}
+
+export default ChannelContainer
